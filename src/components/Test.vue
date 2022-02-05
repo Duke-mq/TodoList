8c7000eb4b88911d2b11388935a464d41f713037
许绵渠 <294848617@qq.com>
2022-02-05 19:51:14 +0800

    基础版TodoList

---
+++ src/components/Test.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="example-5">
+    <select v-model="selected">
+      <option disabled value="">请选择</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+    <span>Selected: {{ selected }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+     selected: ''
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
